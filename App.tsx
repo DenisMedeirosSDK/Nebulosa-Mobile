f17f0e53e8d270b742598f826d9b211f83c7ad1f
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-07-01 15:23:45 -0300

    feat: create routes screens

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 
-import { SignIn } from './src/screens/SignIn';
+import { Routes } from './src/routes';
 
 export default function App() {
   return (
@@ -11,7 +11,7 @@ export default function App() {
         translucent
         backgroundColor="transparent"
       />
-      <SignIn />
+      <Routes />
     </>
   );
 }
